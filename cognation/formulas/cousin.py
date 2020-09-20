ed4c202dc42ad2d706e21a7d3a3f066d5bd43d0f
VieleSchritte <ohheavenineed@gmail.com>
2020-09-20 22:50:54 +0300

    formula for cousins fixed

--- cognation/formulas/cousin.py
+++ cognation/formulas/cousin.py
@@ -14,35 +14,31 @@ class CousinFormula(Formula):
         if len(cousin1_alleles) != 2 or len(cousin2_alleles) != 2:
             raise AllelesException()
 
-        cousin1_set = set(cousin1_alleles)
-        cousin2_set = set(cousin2_alleles)
-        intersection_list = list(cousin1_set & cousin2_set)
-        freq_dict = self.get_frequencies(locus, intersection_list)
+        cousin1_set = set(cousin1_alleles)  # unique cousin 1 alleles
+        cousin2_set = set(cousin2_alleles)  # unique cousin 2 alleles
+        intersection = list(cousin1_set & cousin2_set)  # unique common alleles
 
-        # case1 - no common alleles
-        if len(intersection_list) == 0:
+        cousin1_set_len = len(cousin1_set)
+        cousin2_set_len = len(cousin2_set)
+        inter_len = len(intersection)
+
+        freq_dict = self.get_frequencies(locus, intersection)
+
+        if inter_len == 0:
+            # no common alleles
             lr = 0.75
+        elif inter_len == 2:
+            # borh are heterozygous, two common alleles
+            freq1 = freq_dict[intersection[0]]
+            freq2 = freq_dict[intersection[1]]
+            lr = 0.75 + 0.125 * (freq1 + freq2) / (2 * freq1 * freq2)
+        elif inter_len == 1:
+            freq = freq_dict[intersection[0]]
+            if cousin1_set_len == cousin2_set_len == 1:
+                lr = 0.75 + 0.25 / freq
+            elif cousin1_set_len == 1 and cousin2_set_len == 2:
+                lr = 0.75 + 0.125 / freq
+            elif cousin1_set_len == cousin2_set_len == 2:
+                lr = 0.75 + 0.125 / (2 * freq)
 
-        # case2 - two common alleles, both are heterozygous
-        elif len(intersection_list) == 2:
-            freq1 = freq_dict[intersection_list[0]]
-            freq2 = freq_dict[intersection_list[1]]
-            lr = 0.75 + 0.125*(freq1 + freq2)/(2 * freq1 * freq2)
-
-        # only one intersection - one common allele
-        elif len(intersection_list) == 1:
-            freq = freq_dict[intersection_list[0]]
-
-            # case 3 - both are homozygous
-            if len(cousin1_set) == len(cousin2_set) == 1:
-                lr = 0.75 + 0.25/freq
-
-            # case4 - one of them is heterozygous
-            elif len(cousin1_set) == 1 and len(cousin2_set) == 2:
-                lr = 0.75 + 0.125/freq
-
-            # case 5 - both are the same heterozyhous
-            elif len(cousin1_set) == len(cousin2_set) == 2:
-                lr = 0.75 + 0.125/(2 * freq)
-
-            return self.make_result(locus, '/'.join(cousin1_alleles), '/'.join(cousin2_alleles), lr)
+        return self.make_result (locus, '/'.join(cousin1_alleles), '/'.join(cousin2_alleles), lr)
