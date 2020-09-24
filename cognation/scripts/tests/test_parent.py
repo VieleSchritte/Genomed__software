719f97641a649e64fef6ba8a648d8da8ba7bc9b5
VieleSchritte <ohheavenineed@gmail.com>
2020-09-24 23:13:36 +0300

    first organisation of tests for ParentFormula

--- cognation/scripts/tests/test_parent.py
+++ cognation/scripts/tests/test_parent.py
@@ -1,36 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.test import TestCase
+# from django.test import TestCase
+import unittest
 from ...formulas.parent import ParentFormula
-from ...formulas.base import Formula
 
-
-class TestParentFormula(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
-
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
-
-    doc_name1 = 'test_data_parent1.txt'
-    doc_name2 = 'test_data_parent2.txt'
-
-    # getting loci and lrs in the dictionary and also CPI and P
-    def get_data_parentx(self, doc_name):
-        test_dict = {}
+"""
+class TestParentFormula(unittest.TestCase):
+    # getting loci and lrs in the dictionary and also CPI and P from each patient's reference data
+    def get_reference_data_parentx(self, doc_name):
+        ref_dict = {}
         cpi = 0
         p = 0.0
-        with open('cognation/scripts/tests/test_cases/parent_cases/'+doc_name, 'r') as parent1_data:
-            for line in parent1_data:
+        with open('cognation/scripts/tests/test_cases/parent_cases/'+doc_name, 'r') as parentx_data:
+            for line in parentx_data:
                 line = line.strip().split('\t')
                 if len(line) != 1:
                     locus = line[0]
                     lr = line[3]
-                    test_dict[locus] = lr
+                    ref_dict[locus] = lr
                 else:
                     line = line[0].split('=')
                     if line[0] == 'CPI':
@@ -38,12 +25,91 @@ class TestParentFormula(TestCase):
                     elif line[0] == 'P':
                         self.p = float(line[1])
 
-        return test_dict, cpi, p
+        return ref_dict, cpi, p
 
-    loci_lrs_dict_parent1 = get_data_parentx(doc_name1)[0]
-    cpi_parent1 = get_data_parentx(doc_name1)[1]
-    p_parent1 = get_data_parentx(doc_name1)[2]
 
-    loci_lrs_dict_parent2 = get_data_parentx(doc_name2)[0]
-    cpi_parent2 = get_data_parentx(doc_name2)[1]
-    p_parent2 = get_data_parentx(doc_name2)[2]
+    # getting similar data from each patient's test data
+    def get_test_data_parentx(self, doc_name):
+        test_dict = {}
+        test_cpi = 1
+        with open('cognation/scripts/tests/test_cases/parent_cases/'+doc_name, 'r') as parentx:
+            for line in parentx:
+                parent_formula_dict = ParentFormula.calculate_relation(line)
+                locus = parent_formula_dict['locus']
+                lr = float(parent_formula_dict['lr'])
+                test_cpi *= lr
+                test_dict[locus] = lr
+
+        test_cpi = int(test_cpi)
+        test_p = (test_cpi / (1 + test_cpi)) * 100
+
+        return test_dict, test_cpi, test_p
+
+    # all possible test cases
+    doc_names_list = ['parent1/reference_data_parent1.txt', 'parent2/reference_data_parent2.txt',
+                          'parent3/reference_data_parent3_veri.txt']
+        
+    #getting reference and test data for all parents
+    overall_ref_dict = {}
+    overall_test_dict = {}
+    for i in range(len(doc_names_list)):
+        parent_path = doc_names_list[i]
+        overall_test_dict[parent_path] = get_reference_data_parentx(parent_path)
+        overall_ref_dict[parent_path] = get_test_data_parentx(parent_path)
+"""
+
+class GetParentsData(ParentFormula):
+    # getting loci and lrs in the dictionary and also CPI and P from each patient's reference data
+    def get_reference_data_parentx(self, doc_name):
+        ref_dict = {}
+        cpi = 0
+        p = 0.0
+        with open('cognation/scripts/tests/test_cases/parent_cases/' + doc_name, 'r') as parentx_data:
+            for line in parentx_data:
+                line = line.strip().split('\t')
+                if len(line) != 1:
+                    locus = line[0]
+                    lr = line[3]
+                    ref_dict[locus] = lr
+                else:
+                    line = line[0].split('=')
+                    if line[0] == 'CPI':
+                        cpi = int(line[1])
+                    elif line[0] == 'P':
+                        self.p = float(line[1])
+
+        return ref_dict, cpi, p
+
+    # getting similar data from each patient's test data
+    def get_test_data_parentx(self, doc_name):
+        test_dict = {}
+        test_cpi = 1
+        with open('cognation/scripts/tests/test_cases/parent_cases/' + doc_name, 'r') as parentx:
+            for line in parentx:
+                parent_formula_dict = ParentFormula.calculate_relation(self, line)
+                locus = parent_formula_dict['locus']
+                lr = float(parent_formula_dict['lr'])
+                test_cpi *= lr
+                test_dict[locus] = lr
+
+        test_cpi = int(test_cpi)
+        test_p = (test_cpi / (1 + test_cpi)) * 100
+
+        return test_dict, test_cpi, test_p
+
+
+
+# all possible test cases
+doc_names_list = ['parent1/reference_data_parent1.txt', 'parent2/reference_data_parent2.txt',
+                      'parent3/reference_data_parent3_veri.txt']
+
+# getting reference and test data for all parents
+overall_ref_dict = {}
+overall_test_dict = {}
+for i in range(len(doc_names_list)):
+    parent_path = doc_names_list[i]
+    overall_test_dict[parent_path] = GetParentsData.get_reference_data_parentx(parent_path)
+    overall_ref_dict[parent_path] = GetParentsData.get_test_data_parentx(parent_path)
+
+class TestParentsData(unittest.TestCase):
+    def
