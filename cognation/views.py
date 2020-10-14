31f79080899edeff1b4886bb977fdff6c6362419
VieleSchritte <ohheavenineed@gmail.com>
2020-10-14 11:14:52 +0300

    fixed gender-specific loci output

--- cognation/views.py
+++ cognation/views.py
@@ -84,7 +84,7 @@ def make_order_cordis():
 
 
 def make_order_verifiler():
-    return ['AMEL', 'Yindel', 'D3S1358', 'vWA', 'D16S539', 'CSF1PO', 'TPOX', 'D8S1179', 'D21S11', 'D18S51', 'Penta E', 'D2S441',
+    return ['AMEL', 'Yindel', 'DYS391', 'SRY', 'D3S1358', 'vWA', 'D16S539', 'CSF1PO', 'TPOX', 'D8S1179', 'D21S11', 'D18S51', 'Penta E', 'D2S441',
             'D19S433', 'TH01', 'FGA', 'D22S1045', 'D5S818', 'D13S317', 'D7S820', 'D6S1043', 'D10S1248', 'D1S1656',
             'D12S391', 'D2S1338', 'Penta D']
 
