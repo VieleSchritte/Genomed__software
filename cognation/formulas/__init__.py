0c307f4532d8bde75192abd102119af412a1474a
VieleSchritte <ohheavenineed@gmail.com>
2020-10-26 17:37:41 +0300

    new formula for 2 children

--- cognation/formulas/__init__.py
+++ cognation/formulas/__init__.py
@@ -7,6 +7,7 @@ from cognation.formulas.brother import BrotherFormula
 from cognation.formulas.sibling import SiblingFormula
 from cognation.formulas.two_parents import TwoParentsFormula
 from cognation.formulas.base import UnknownFormulaException
+from cognation.formulas.two_children import TwoChildrenFormula
 
 
 numToFormula = {
@@ -17,7 +18,8 @@ numToFormula = {
     5: StepbrotherFormula,
     6: BrotherFormula,
     7: SiblingFormula,
-    8: TwoParentsFormula
+    8: TwoParentsFormula,
+    9: TwoChildrenFormula
 }
 
 
--- cognation/formulas/two_children.py
+++ cognation/formulas/two_children.py
@@ -4,7 +4,7 @@ from .base import Calculations
 from .parent import ParentFormula
 
 
-class TwoChildren(Formula):
+class TwoChildrenFormula(Formula):
     def calculate_relation(self, raw_values):
         (locus, part_alleles, part_sets, intersections, dict_make_result) = self.getting_alleles_locus(raw_values, 3)
         child1_alleles, child2_alleles, parent_alleles = part_alleles
@@ -24,29 +24,44 @@ class TwoChildren(Formula):
         if len(ch1p_intersection) >= 1 and len(ch2p_intersection) >= 1:
             # Homozygous 1st child
             if len(child1_set) == 1:
+
                 # case aa an an
-                if ch2p_intersection == ch1p_intersection:
+                if len(ch1ch2_intersection) != 0:
                     lr = c.F(freq_dict[child1_alleles[0]])
                     return self.make_result(locus, lr, dict_make_result)
 
-                common_set = set(child1_alleles + child2_alleles + parent_set)
-                # case aa bb ab
-                if len(common_set) == 2:
-                    freq1, freq2 = freq_dict[list(common_set)[0]], freq_dict[list(common_set)[1]]
-                    lr = 2 * freq1 * freq2
-                    return self.make_result(locus, lr, dict_make_result)
+                else:
+                    # case aa bb ab
+                    if len(child2_set) == 1:
+                        freq1, freq2 = freq_dict[child1_alleles[0]], freq_dict[child2_alleles[0]]
+                        lr = 2 * freq1 * freq2
+                        return self.make_result(locus, lr, dict_make_result)
 
-                # case aa bc ab/ac
-                if len(common_set) == 3:
-                    freq1 = freq_dict[list(ch1p_intersection)]
-                    unique_allele1 = c.get_unique_allele(parent_alleles, child1_alleles)
-                    freq2 = freq_dict[unique_allele1]
-                    unique_allele2 = c.get_unique_allele(list(common_set), [unique_allele1, list(ch1p_intersection)])
-                    freq3 = freq_dict[unique_allele2]
-                    lr = 2 * freq1 * (freq2 + freq3)
-                    return self.make_result(locus, lr, dict_make_result)
+                    # case aa bc ab/ac
+                    else:
+                        freq1, freq2, freq3 = freq_dict[child1_alleles[0]], freq_dict[child2_alleles[0]], freq_dict[child2_alleles[1]]
+                        lr = 2 * freq1 * (freq2 + freq3)
+                        return self.make_result(locus, lr, dict_make_result)
 
             # Heterozygous 1st child
             else:
+                # case ab cc ac/bc
                 if len(child2_set) == 1:
-                    
+                    freq1, freq2, freq3 = freq_dict[child2_alleles[0]], freq_dict[child1_alleles[0]], freq_dict[child1_alleles[1]]
+                    lr = 2 * freq1 * (freq2 + freq3)
+                    return self.make_result(locus, lr, dict_make_result)
+
+                # case ab ac an/bc
+                if len(child2_set) == 2 and len(ch1ch2_intersection) == 1:
+                    freq2, freq3 = freq_dict[child1_alleles[0]], freq_dict[child1_alleles[1]]
+                    lr = c.F(freq_dict[list(ch1ch2_intersection)[0]]) + 2 * freq2 * freq3
+                    return self.make_result(locus, lr, dict_make_result)
+
+                # case ab cd ac/ad/bc/bd
+                if len(child2_set) == 2 and len(ch1ch2_intersection) == 0:
+                    freq1, freq2 = freq_dict[child1_alleles[0]], freq_dict[child1_alleles[1]]
+                    freq3, freq4 = freq_dict[child2_alleles[0]], freq_dict[child2_alleles[1]]
+                    lr = 2 * (freq1 + freq2) * (freq3 + freq4)
+                    return self.make_result(locus, lr, dict_make_result)
+
+        return self.make_result(locus, lr, dict_make_result)
