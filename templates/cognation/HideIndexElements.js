345bd96e77ff34f5b91a76e919ab62f204c3f28c
VieleSchritte <ohheavenineed@gmail.com>
2020-11-11 19:36:18 +0300

    working new structure

--- templates/cognation/HideIndexElements.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-function show() {
-    let allDivs = document.getElementsByClassName('content');
-    for (let i = 0; i < allDivs.length; i++) {
-        allDivs[i].hidden = allDivs[i].hidden != true;
-    }
-    //document.getElementById('content' + id).classList.remove('hidden');
-}
