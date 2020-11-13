8b5d3b0498f43f7de865e095cf1e8657ec898803
VieleSchritte <ohheavenineed@gmail.com>
2020-11-13 11:28:27 +0300

    trashhhh

--- genomed_software/settings.py
+++ genomed_software/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "sfiles"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
