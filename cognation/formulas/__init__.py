63277870cf4c003465af1b5c73320f517e2d53b5
VieleSchritte <ohheavenineed@gmail.com>
2020-11-16 19:38:18 +0300

    workable but not tested formula for three children and a couple

--- cognation/formulas/__init__.py
+++ cognation/formulas/__init__.py
@@ -15,6 +15,7 @@ from cognation.formulas.two_known_supposed import TwoKnownSupposedFormula
 from cognation.formulas.two_brothers import TwoBrothersFormula
 from cognation.formulas.three_known_supposed import ThreeKnownSupposed
 from cognation.formulas.two_couple import TwoCoupleFormula
+from cognation.formulas.three_couple import ThreeCoupleFormula
 
 
 numToFormula = {
@@ -27,7 +28,7 @@ numToFormula = {
     7: CoupleFormula,
     8: OneKnownSupposedFormula,
     9: TwoCoupleFormula,
-    10: [],
+    10: ThreeCoupleFormula,
     11: StepbrotherFormula,
     12: BrotherFormula,
     13: TwoBrothersFormula,
