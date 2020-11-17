7a50610d6aaebe703d971080320e673d4e405d95
VieleSchritte <ohheavenineed@gmail.com>
2020-11-17 20:27:22 +0300

    tried to make a correct way to static files, but nothing seems to work. Started a function HideFormulaButtons which should make unclicked buttons disappear and make description visible. It works partly((((

--- cognation/urls.py
+++ cognation/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
-app_name='cognation'
+app_name = 'cognation'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^calculate/$', views.calculate, name='calculate'),
     url(r'^save_allele/', views.save_allele, name='save_allele')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- genomed_software/settings.py
+++ genomed_software/settings.py
@@ -133,4 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "sfiles"),
     os.path.join(BASE_DIR, "static/css"),
+    os.path.join(BASE_DIR, "static/button_images"),
 )
