655b0618cec2d8833aba11fb19f005229e419939
VieleSchritte <ohheavenineed@gmail.com>
2020-11-18 14:52:05 +0300

    FINALLY WORKABLE STATIC

--- genomed_software/settings.py
+++ genomed_software/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "sfiles"),
     os.path.join(BASE_DIR, "static/css"),
     os.path.join(BASE_DIR, "static/button_images"),
 )
