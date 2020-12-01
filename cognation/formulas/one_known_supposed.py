dedc63499e51fa792e2a614dbdbd6c69ff16d653
VieleSchritte <ohheavenineed@gmail.com>
2020-12-01 16:56:14 +0300

    fixed logic in one known supposed, in views tried to fixed probability calculation. This part is in process

--- cognation/formulas/one_known_supposed.py
+++ cognation/formulas/one_known_supposed.py
@@ -12,19 +12,37 @@ class OneKnownSupposedFormula(Formula):
         if self.is_gender_specific(locus):
             return self.make_result(locus, '-', dict_make_result)
 
+        if locus == 'AMEL':
+            return self.make_result(locus, 1, dict_make_result)
+
         lr = 0
         if len(sch_inter) == 0 or len(kch_inter) == 0:
             return self.make_result(locus, lr, dict_make_result)
 
         freq_dict = self.get_frequencies(locus, child_alleles + known_alleles + sup_alleles)
+        print(locus)
+        print('child, known, sup: ', child_alleles, known_alleles, sup_alleles)
+        print(freq_dict)
         freq1, freq2 = freq_dict[child_alleles[0]], freq_dict[child_alleles[1]]
 
         # cases ab ab an/bn
-        if child_set == known_set:
+        if child_set == known_set and len(child_set) == 2:
             lr = (freq1 + freq2) * (2 - (freq1 + freq2))
+            print(lr)
+            print()
+            return self.make_result(locus, lr, dict_make_result)
+
+        c = Calculations()
+        # case ab an bn
+        if len(child_set) == 2:
+            freq = freq_dict[list(child_set - known_set)[0]]
+            lr = c.F(freq)
+            print(lr)
+            print()
             return self.make_result(locus, lr, dict_make_result)
 
         freq = freq_dict[list(sch_inter)[0]]
-        c = Calculations()
         lr = c.F(freq)
+        print(lr)
+        print()
         return self.make_result(locus, lr, dict_make_result)
--- cognation/views.py
+++ cognation/views.py
@@ -18,6 +18,7 @@ def calculate(request):
     data = request.POST.get('data', '')
 
     formula = formula_builder(data_type, data)
+    print('==========================', formula)
     result = formula.calculate()
 
     cpi = 1
@@ -33,6 +34,11 @@ def calculate(request):
                 cpi *= result[key]["lr"]
             else:
                 mutations += 1
+    print('===========CPI = ', cpi)
+    print('===========P = ', (cpi / (1. + cpi)) * 100.)
+    print()
+    print('===========CPI Hardy = ', 1 / cpi)
+    print('===========1 - P Hardy = ', (1 - cpi)*100)
 
     if mutations > 2:
         cpi = 0
